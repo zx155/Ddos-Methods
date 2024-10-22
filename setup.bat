6b1f5e7986668ff12c34714b06115083ed2720b0
zx155 <168565589+zx155@users.noreply.github.com>
2024-10-22 19:53:16 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,10 +1,10 @@
-import threading
-import asyncio
-import httpx
-import socket
-import os
-import time
-import random
-import sys
-import json
+pip install threading
+pip install asyncio
+pip install httpx
+pip install socket
+pip install os
+pip install time
+pip install random
+pip install sys
+pip install json
 pip install pybase64
